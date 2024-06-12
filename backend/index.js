53ef4e72994382f64ee626cf2459dc8515db9958
Adamauer1 <adamauer1@gmail.com>
2024-06-13 00:11:11 +0200

    adding basic login
    Able to log in with premade values in the backend.
    NEED TO PUT IN OWN URL to connect with the backend it will be the ip of where the backend server is running

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,31 @@
 const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 const data = require("./data.js");
 const app = express();
 const port = 3000;
 
+app.use(cors());
+app.use(bodyParser.json());
+
 app.get("/", (req, res) => {
   res.json(data);
+});
+
+app.post("/login", (req, res) => {
+  const { username, password } = req.body;
+  console.log(username);
+  console.log(password);
+  let authCheck = false;
+  let userID = -1;
+  for (user of data.users) {
+    if (user.username === username && user.password === password) {
+      authCheck = true;
+      userID = user.userID;
+      break;
+    }
+  }
+  res.json({ userID: userID, result: authCheck });
 });
 
 app.listen(port, () => {
--- frontend/app/login.tsx
+++ frontend/app/login.tsx
@@ -1,13 +1,69 @@
-//import * as React from "react";
-import { View, StyleSheet, Image } from "react-native";
+import { useEffect, useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Image,
+  TextInput,
+  Text,
+  Pressable,
+} from "react-native";
+import { Link, router } from "expo-router";
+import axios from "axios";
 //import TextButton from "./TextButton.tsx";
-
+const URL = "";
 export default function LogInScreen({}) {
   //export default function GetStartedScreen({ navigation }) {
+  const [username, onChangeUsername] = useState("");
+  const [password, onChangePassword] = useState("");
+  //   const [checkLogin, setCheckLogin] = useState(false);
+  //   const [loginResult, setLoginResult] = useState(false);
+
+  const onPressLogin = async () => {
+    // send post to server with username and password
+    // check on server if valid
+    // return result by moving to next page or displaying error
+    //console.log(username);
+    const data = { username: username, password: password };
+    axios
+      .post(`http://${URL}:3000/login`, {
+        username,
+        password,
+      })
+      .then((res) => {
+        console.log(res.data);
+        if (res.data.result) {
+          // navigate to next page
+          router.replace({
+            pathname: "/main",
+            params: { userID: res.data.userID },
+          });
+          console.log("test");
+        } else {
+          // display error message
+        }
+      })
+      .catch((error) => {});
+  };
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
-        <Image source={require("../images/Main.png")} style={styles.image} />
+        {/* <Image source={require("@/images/Main.png")} style={styles.image} /> */}
+        <Text style={styles.text}>Username</Text>
+        <TextInput
+          style={styles.input}
+          value={username}
+          onChangeText={onChangeUsername}
+        />
+        <Text style={styles.text}>Password</Text>
+        <TextInput
+          style={styles.input}
+          value={password}
+          onChangeText={onChangePassword}
+          secureTextEntry={true}
+        />
+        <Pressable style={styles.button} onPress={onPressLogin}>
+          <Text>Login</Text>
+        </Pressable>
       </View>
     </View>
   );
@@ -33,6 +89,20 @@ const styles = StyleSheet.create({
     marginTop: 20,
     marginBottom: 100,
   },
+  input: {
+    height: 60,
+    width: 250,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    backgroundColor: "#afee",
+    textAlign: "center",
+  },
+
+  text: {
+    color: "white",
+  },
+
   background: {
     flex: 1,
     resizeMode: "cover", // or 'stretch'
